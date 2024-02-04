c454de6624b93107cd7196add7e968bc00664eaa
riccardotesta88 <apple.newton.testa@gmail.com>
2024-02-04 11:50:36 +0100

    Creazione modello base iscritti Aggiunta a administrator

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,3 +8,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Iscritti(models.Model):
+    class Meta:
+        verbose_name="Iscritti"
+        verbose_name_plural="Iscritti"
+
+    # dati genari della persona
+    nome=models.CharField(max_length=50, null=False)
+    cognome=models.CharField(max_length=50, null=False)
+    cf=models.CharField(max_length=16,null=False)
+    data_nascita=models.DateField(null=True)
+
+    # Dati del recapito della persona
+    res_comune=models.CharField(max_length=50)
+    res_recapito=models.CharField(max_length=150)
+
+
